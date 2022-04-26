717f49f0e4462e3d3c8366f30567c02bf1c4fedc
eliashz <readmyspine@gmail.com>
2022-04-26 12:11:41 +0200

    7u Ok

--- query_universidad.sql
+++ query_universidad.sql
@@ -1,3 +1,11 @@
 USE universidad;
 
-/* 6 */  SELECT apellido1, apellido2, persona.nombre, departamento.nombre FROM persona JOIN profesor ON persona.id = profesor.id_profesor JOIN departamento ON profesor.id_departamento = departamento.id ORDER BY  apellido2 ASC, persona.nombre ASC;
+/* 7 */  
+SELECT persona.nif, asignatura.nombre, curso_escolar.anyo_incio
+FROM persona
+JOIN alumno_se_matricula_asignatura
+ON  persona.id = alumno_se_matricula_asignatura.id_alumno
+JOIN asignatura
+ON alumno_se_matricula_asignatura.id_asignatura = asignatura.id
+JOIN curso_escolar
+ON curso_escolar.id = alumno_se_matricula_asignatura.id_curso_escolar;
