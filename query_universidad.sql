d845b61a2e85c214894e8f48c695cd10024d0ef3
eliashz <readmyspine@gmail.com>
2022-04-26 13:07:10 +0200

    8 fet

--- query_universidad.sql
+++ query_universidad.sql
@@ -1,3 +1,7 @@
 USE universidad;
 
-/* 7 */ SELECT asignatura.nombre, ce.anyo_inicio FROM persona JOIN alumno_se_matricula_asignatura aa ON  persona.id = aa.id_alumno JOIN asignatura ON aa.id_asignatura = asignatura.id JOIN curso_escolar ce ON ce.id = aa.id_curso_escolar WHERE persona.nif = '26902806M';
+/* 8 */ SELECT DISTINCT p.nombre 
+FROM  persona p
+JOIN alumno_se_matricula_asignatura aa
+ON aa.id_alumno = p.id 
+WHERE aa.id_curso_escolar = 5;
--- schema_universidad.sql
+++ schema_universidad.sql
@@ -265,13 +265,6 @@ USE universidad;
 /* 3 */ SELECT * FROM persona WHERE tipo = 'alumno' AND fecha_nacimiento BETWEEN '1999-01-01' AND '1999-12-31';
 /* 4 */ SELECT * FROM persona WHERE tipo = 'profesor' AND telefono IS NULL AND nif LIKE '%k';
 /* 5 */ SELECT nombre FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
-/* 6 */  SELECT apellido1, apellido2, persona.nombre, departamento.nombre FROM persona JOIN profesor ON persona.id = profesor.id_profesor JOIN departamento ON profesor.id_departamento = departamento.id ORDER BY apellido1 ASC, apellido2 ASC, persona.nombre ASC;
-SELECT asignatura.nombre, curso_escolar.anyo_inicio
-FROM persona
-JOIN alumno_se_matricula_asignatura
-ON  persona.id = alumno_se_matricula_asignatura.id_alumno
-JOIN asignatura
-ON alumno_se_matricula_asignatura.id_asignatura = asignatura.id
-JOIN curso_escolar
-ON curso_escolar.id = alumno_se_matricula_asignatura.id_curso_escolar
-WHERE persona.nif = '26902806M';
+/* 6 */ SELECT apellido1, apellido2, persona.nombre, departamento.nombre FROM persona JOIN profesor ON persona.id = profesor.id_profesor JOIN departamento ON profesor.id_departamento = departamento.id ORDER BY apellido1 ASC, apellido2 ASC, persona.nombre ASC;
+/* 7 */ SELECT asignatura.nombre, ce.anyo_inicio FROM persona JOIN alumno_se_matricula_asignatura aa ON  persona.id = aa.id_alumno JOIN asignatura ON aa.id_asignatura = asignatura.id JOIN curso_escolar ce ON ce.id = aa.id_curso_escolar WHERE persona.nif = '26902806M';
+/* 8 */ SELECT DISTINCT p.nombre FROM  persona p JOIN alumno_se_matricula_asignatura aaON aa.id_alumno = p.id WHERE aa.id_curso_escolar = 5;
